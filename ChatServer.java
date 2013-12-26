bf370ce2bc7ea5fdabd38d1744001254770f2aae
ChronoControl <ricardo.v.borga@gmail.com>
2013-12-26 14:25:04 +0000

    Server do borga etc

--- ChatServer.java
+++ ChatServer.java
@@ -5,176 +5,200 @@ import java.nio.channels.*;
 import java.nio.charset.*;
 import java.util.*;
 
+
+
 public class ChatServer
 {
-        // A pre-allocated buffer for the received data
-        static private final ByteBuffer buffer = ByteBuffer.allocate( 16384 );
+	// A pre-allocated buffer for the received data
+	static private final ByteBuffer buffer = ByteBuffer.allocate( 16384 );
 
-        // Decoder for incoming text -- assume UTF-8
-        static private final Charset charset = Charset.forName("UTF8");
-        static private final CharsetDecoder decoder = charset.newDecoder();
+	// Decoder for incoming text -- assume UTF-8
+	static private final Charset charset = Charset.forName("UTF8");
+	static private final CharsetDecoder decoder = charset.newDecoder();
+	
+	public static Hashtable<SocketChannel, String> users = new Hashtable<SocketChannel, String>();
+	
+	static public void main( String args[] ) throws Exception {
+		// Parse port from command line
+		//int port = Integer.parseInt( args[0] );
+		
+		
+		Hashtable channels = new Hashtable();
+		
+		int port = 10002;
+		try {
+			// Instead of creating a ServerSocket, create a ServerSocketChannel
+			ServerSocketChannel ssc = ServerSocketChannel.open();
 
+			// Set it to non-blocking, so we can use select
+			ssc.configureBlocking( false );
 
-        static public void main( String args[] ) throws Exception {
-                // Parse port from command line
-                //int port = Integer.parseInt( args[0] );
-                int port = Integer.parseInt( args[0] );
-                try {
-                        // Instead of creating a ServerSocket, create a ServerSocketChannel
-                        ServerSocketChannel ssc = ServerSocketChannel.open();
+			// Get the Socket connected to this channel, and bind it to the
+			// listening port
+			ServerSocket ss = ssc.socket();
+			InetSocketAddress isa = new InetSocketAddress( port );
+			ss.bind( isa );
 
-                        // Set it to non-blocking, so we can use select
-                        ssc.configureBlocking( false );
+			// Create a new Selector for selecting
+			Selector selector = Selector.open();
 
-                        // Get the Socket connected to this channel, and bind it to the
-                        // listening port
-                        ServerSocket ss = ssc.socket();
-                        InetSocketAddress isa = new InetSocketAddress( port );
-                        ss.bind( isa );
+			// Register the ServerSocketChannel, so we can listen for incoming
+			// connections
+			ssc.register( selector, SelectionKey.OP_ACCEPT );
+			System.out.println( "Listening on port "+ port );
 
-                        // Create a new Selector for selecting
-                        Selector selector = Selector.open();
+			while (true) {
+				// See if we've had any activity -- either an incoming connection,
+				// or incoming data on an existing connection
+				int num = selector.select();
 
-                        // Register the ServerSocketChannel, so we can listen for incoming
-                        // connections
-                        ssc.register( selector, SelectionKey.OP_ACCEPT );
-                        System.out.println( "Listening on port "+ port );
+				// If we don't have any activity, loop around and wait again
+				if (num == 0) {
+					continue;
+				}
 
-                        while (true) {
-                                // See if we've had any activity -- either an incoming connection,
-                                // or incoming data on an existing connection
-                                int num = selector.select();
+				// Get the keys corresponding to the activity that has been
+				// detected, and process them one by one
+				Set keys = selector.selectedKeys();
+				Iterator it = keys.iterator();
+				while (it.hasNext()) {
+					// Get a key representing one of bits of I/O activity
+					SelectionKey key = (SelectionKey)it.next();
 
-                                // If we don't have any activity, loop around and wait again
-                                if (num == 0) {
-                                        continue;
-                                }
+					// What kind of activity is it?
+					if (key.isAcceptable()) {
 
-                                // Get the keys corresponding to the activity that has been
-                                // detected, and process them one by one
-                                Set keys = selector.selectedKeys();
-                                Iterator it = keys.iterator();
-                                while (it.hasNext()) {
-                                        // Get a key representing one of bits of I/O activity
-                                        SelectionKey key = (SelectionKey)it.next();
+						System.out.println("TESTE");
+						// It's an incoming connection.  Register this socket with
+						// the Selector so we can listen for input on it
+						Socket s = ss.accept();
+						System.out.println( "Got connection from "+s );
 
-                                        // What kind of activity is it?
-                                        if (key.isAcceptable()) {
+						// Make sure to make it non-blocking, so we can use a selector
+						// on it.
+						SocketChannel sc = s.getChannel();
+						sc.configureBlocking( false );
 
-                                                // It's an incoming connection.  Register this socket with
-                                                // the Selector so we can listen for input on it
-                                                Socket s = ss.accept();
-                                                System.out.println( "Got connection from "+s );
+						// Register it with the selector, for reading
+						sc.register( selector, SelectionKey.OP_READ );
+						users.put(sc, "asd");
+					} else if (key.isReadable()) {
 
-                                                // Make sure to make it non-blocking, so we can use a selector
-                                                // on it.
-                                                SocketChannel sc = s.getChannel();
-                                                sc.configureBlocking( false );
+						SocketChannel sc = null;
 
-                                                // Register it with the selector, for reading
-                                                sc.register( selector, SelectionKey.OP_READ );
+						try {
 
-                                        } else if (key.isReadable()) {
+							// It's incoming data on a connection -- process it
+							sc = (SocketChannel)key.channel();
+							boolean ok = processInput( sc );
 
-                                                SocketChannel sc = null;
+							// If the connection is dead, remove it from the selector
+							// and close it
+							if (!ok) {
+								key.cancel();
 
-                                                try {
+								Socket s = null;
+								try {
+									s = sc.socket();
+									System.out.println( "Closing connection to "+s );
+									s.close();
+								} catch( IOException ie ) {
+									System.err.println( "Error closing socket "+s+": "+ie );
+								}
+							}
 
-                                                        // It's incoming data on a connection -- process it
-                                                        sc = (SocketChannel)key.channel();
-                                                        boolean ok = processInput( sc );
+						} catch( IOException ie ) {
 
-                                                        // If the connection is dead, remove it from the selector
-                                                        // and close it
-                                                        if (!ok) {
-                                                                key.cancel();
+							// On exception, remove this channel from the selector
+							key.cancel();
 
-                                                                Socket s = null;
-                                                                try {
-                                                                        s = sc.socket();
-                                                                        System.out.println( "Closing connection to "+s );
-                                                                        s.close();
-                                                                } catch( IOException ie ) {
-                                                                        System.err.println( "Error closing socket "+s+": "+ie );
-                                                                }
-                                                        }
+							try {
+								sc.close();
+							} catch( IOException ie2 ) { System.out.println( ie2 ); }
 
-                                                } catch( IOException ie ) {
+							System.out.println( "Closed "+sc );
+						}
+					}
+				}
 
-                                                        // On exception, remove this channel from the selector
-                                                        key.cancel();
-
-                                                        try {
-                                                                sc.close();
-                                                        } catch( IOException ie2 ) { System.out.println( ie2 ); }
-
-                                                        System.out.println( "Closed "+sc );
-                                                }
-                                        }
-                                }
-
-                                // We remove the selected keys, because we've dealt with them.
-                                keys.clear();
-                        }
-                } catch( IOException ie ) {
-                        System.err.println( ie );
-                }
-        }
-
-
-        // Just read the message from the socket and send it to stdout
-        static private boolean processInput( SocketChannel sc ) throws IOException {
-                // Read the message to the buffer
-
-                Socket s = sc.socket();
-
-                buffer.clear();
-                sc.read( buffer );
-                buffer.flip();
-
-                // If no data, close the connection
-                if (buffer.limit()==0) {
-                        return false;
-                }
-
-                // Decode and print the message to stdout
-                String message = decoder.decode(buffer).toString();
-
-                if(message.charAt(0) == '/') {
+				// We remove the selected keys, because we've dealt with them.
+				keys.clear();
+			}
+		} catch( IOException ie ) {
+			System.err.println( ie );
+		}
+	}
 
 
-                        String[] parsed = message.split(" |\\n");
+	// Just read the message from the socket and send it to stdout
+	static private boolean processInput( SocketChannel sc ) throws IOException {
+		// Read the message to the buffer
 
+		Socket s = sc.socket();
 
-                        // NICK
-                        if (parsed[0].equalsIgnoreCase("/nick") && parsed.length == 2) {
-                                System.out.println("ERROR: The command /nick is not yet implemented");
+		buffer.clear();
+		sc.read( buffer );
+		buffer.flip();
+		
+		// If no data, close the connection
+		if (buffer.limit()==0) {
+			return false;
+		}
 
-                        }
+		// Decode and print the message to stdout
+		String message = decoder.decode(buffer).toString();
 
-                        // JOIN
-                        if (parsed[0].equalsIgnoreCase("/join")) {
-                                System.out.println("ERROR: The command /join is not yet implemented");
-                        }
-                        // LEAVE
-                        if (parsed[0].equalsIgnoreCase("/leave")) {
-                                System.out.println("ERROR: The command /leave is not yet implemented");
-                        }
-                        // BYE
-                        if (parsed[0].equalsIgnoreCase("/bye")) {
-                                System.out.println("ERROR: The command /bye is not yet implemented");
-                        }
-                        // PRIV
-                        if (parsed[0].equalsIgnoreCase("/priv")) {
-                                System.out.println("ERROR: The command /bye is not yet implemented");
-                        }
+		if(message.charAt(0) == '/') {
 
-                }
+			String[] parsed = message.split(" |\\n");
 
+			// NICK
+			if (parsed[0].equalsIgnoreCase("/nick") && parsed.length == 2) {
+				System.out.println("ERROR: The command /nick is not yet implemented");
+				
+				
+				
+				if(users.containsValue(parsed[1])) {
+					System.out.println("nick existente");
+				} else {
+					users.put(sc, parsed[1]);
+					System.out.println("MUDEI DE NICK!!");
+				}
+			}
 
+			// JOIN
+			if (parsed[0].equalsIgnoreCase("/join")) {
+				System.out.println("ERROR: The command /join is not yet implemented");
+			}
+			// LEAVE
+			if (parsed[0].equalsIgnoreCase("/leave")) {
+				System.out.println("ERROR: The command /leave is not yet implemented");
+			}
+			// BYE
+			if (parsed[0].equalsIgnoreCase("/bye")) {
+				System.out.println("ERROR: The command /bye is not yet implemented");
+			}
+			// PRIV
+			if (parsed[0].equalsIgnoreCase("/priv")) {
+				System.out.println("ERROR: The command /bye is not yet implemented");
+			}
 
-                System.out.print( message );
+		}
 
-                return true;
-        }
+		
+		System.out.println(sc.toString());
+		// System.out.print( message );
+		
+		String asd = message + " um texto meu\n";
+		buffer.clear();
+		buffer.put(asd.getBytes());
+		buffer.flip();
+		
+		while(buffer.hasRemaining()) {
+		    sc.write(buffer);
+		}
+		
+		
+		return true;
+	}
 }
